200896855a7d5383746f5b6a2495bfe88f5d97d6
Eswar Andavarapu <eswararao.andavarapu@optiv.com>
2019-08-27 16:23:44 +0530

    Updates

---
+++ precache-manifest.9e9bc6a1e4e8484178c257b0a7f1bf48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7a796f2c62f2eae2c89819d0e096842",
+    "url": "/queen-pawn-puzzle/static/media/index.f7a796f2.less"
+  },
+  {
+    "revision": "eab5285cf2356c2db7b2f69a0ee2160f",
+    "url": "/queen-pawn-puzzle/static/media/index.eab5285c.less"
+  },
+  {
+    "revision": "e6e4b10289e988de87ab126805acd87f",
+    "url": "/queen-pawn-puzzle/static/media/index.e6e4b102.less"
+  },
+  {
+    "revision": "11c759a0d3e3e3370dcf",
+    "url": "/queen-pawn-puzzle/static/js/runtime~main.11c759a0.js"
+  },
+  {
+    "revision": "512c16cf9912dad489e0",
+    "url": "/queen-pawn-puzzle/static/js/main.512c16cf.chunk.js"
+  },
+  {
+    "revision": "3ae6abf5d7721c9b91c5",
+    "url": "/queen-pawn-puzzle/static/js/1.3ae6abf5.chunk.js"
+  },
+  {
+    "revision": "512c16cf9912dad489e0",
+    "url": "/queen-pawn-puzzle/static/css/main.7ff3bb8e.chunk.css"
+  },
+  {
+    "revision": "e3ddbb8650da909de09a1d0c93b5b79c",
+    "url": "/queen-pawn-puzzle/index.html"
+  }
+];
